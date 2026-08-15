b8cca1e99af99958a5c53843c3e11de4366dd19f
Harshal Chavan <harshal24.chavan@gmail.com>
2026-08-15 14:59:55 +0530

    update main and delete old files

--- main.cpp
+++ main.cpp
@@ -1,183 +1,46 @@
-#include "TSQueue.h"
-#include "ThreadPool.h"
-#include "ThreadPoolDeq.h"
+#include "./ChaseLevDeq/CL-WS-Threadpool.hpp"
+#include "./ChaseLevDeq/latency-benchmark.hpp"
+
+// #include "./MutexBasedQueues/Latency-Benchmark.hpp"
 
 #include <cassert>
-#include <chrono>
-#include <iostream>
-#include <memory>
-#include <mutex>
-#include <numeric>
-#include <thread>
-#include <utility>
-#include <vector>
-
-// producer inserting things in the queue
-void producer(std::shared_ptr<TSQueue<int>> q, int id) {
-
-  using namespace std::chrono_literals;
-  for (int i = 1; i <= 100; i++) {
-    q->insert(i * id);
-    std::this_thread::sleep_for(100ms);
-  }
-}
-
-// consumer reading things from queue
-std::mutex consumerMtx;
-void consumer(std::shared_ptr<TSQueue<int>> q, int id) {
-  // int n = 1e6 + 7;
-  while (1) {
-    int val = -1;
-    q->pop(val);
-    // we need lock because cout is not thread safe
-    // the output print will be jumbled without a lock
-    // std::lock_guard<std::mutex> lg(consumerMtx);
-    // std::cout << "Consumer: " << id << " | consumed: " << val << std::endl;
-    // n--;
-  }
-}
-
-void run_unit_tests();
-
-void run_benchmark(int num_producers, int num_consumers,
-                   int items_per_producer) {
-  TSQueue<int> benchQ;
-  int total_items = num_producers * items_per_producer;
-
-  auto start = std::chrono::high_resolution_clock::now();
-
-  // 1. Start Consumers (They will wait for data)
-  std::vector<std::thread> consumers;
-  for (int i = 0; i < num_consumers; ++i) {
-    consumers.emplace_back(
-        [&benchQ, items_per_producer, num_producers, num_consumers]() {
-          // Each consumer takes a fair share
-          int to_pop = (num_producers * items_per_producer) / num_consumers;
-          for (int j = 0; j < to_pop; ++j) {
-            int val;
-            benchQ.pop(val);
-          }
-        });
-  }
-
-  // 2. Start Producers
-  std::vector<std::thread> producers;
-  for (int i = 0; i < num_producers; ++i) {
-    producers.emplace_back([&benchQ, items_per_producer]() {
-      for (int j = 0; j < items_per_producer; ++j) {
-        benchQ.insert(j);
-      }
-    });
-  }
-
-  for (auto &t : producers)
-    t.join();
-  for (auto &t : consumers)
-    t.join();
-
-  auto end = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double> elapsed = end - start;
-
-  double throughput = total_items / elapsed.count();
-
-  std::cout << "--- Benchmark Results ---" << std::endl;
-  std::cout << "Threads: " << (num_producers + num_consumers) << std::endl;
-  std::cout << "Total Items: " << total_items << std::endl;
-  std::cout << "Time: " << elapsed.count() << "s" << std::endl;
-  std::cout << "Throughput: " << throughput << " items/sec" << std::endl;
-}
-
-void my_task_wrapper(void *arg) {
-  auto *counter = static_cast<std::atomic<int> *>(arg);
-
-  // Perform dummy work
-  double result = 0;
-  for (int i = 0; i < 1000; ++i) {
-    result += (i * i);
-  }
-
-  (*counter)--;
-}
-
-void benchmark_thread_pool(int num_threads, int total_tasks) {
-  ThreadPoolDeq<Task> pool;
-  std::atomic<int> tasks_remaining(total_tasks);
-
-  Task t;
-  t.func = my_task_wrapper;
-  t.arg = &tasks_remaining;
-
-  auto start = std::chrono::high_resolution_clock::now();
-
-  // using namespace std::chrono_literals;
-  // pool.addTask([&tasks_remaining](){
-  //     std::this_thread::sleep_for(1s);
-  //     });
-
-  for (int i = 0; i < total_tasks; ++i) {
-    pool.submitTask(t);
-  }
-
-  // "Busy wait" until all tasks are done
-  while (tasks_remaining > 0) {
-    std::this_thread::yield();
-  }
-
-  auto end = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double> elapsed = end - start;
-
-  std::cout << "--- ThreadPool Benchmark ---" << std::endl;
-  std::cout << "Threads: " << std::thread::hardware_concurrency()
-            << " | Tasks: " << total_tasks << std::endl;
-  std::cout << "Time: " << elapsed.count() << "s" << std::endl;
-  std::cout << "Throughput: " << total_tasks / elapsed.count() << " tasks/sec"
-            << std::endl;
-}
 
 int main() {
+  WorkStealingScheduler<2, 1024> scheduler;
 
-  benchmark_thread_pool(6, 10000000);
+  global_contexts.resize(TOTAL_TASKS);
+  for (int i = 0; i < TOTAL_TASKS; ++i) {
+    global_contexts[i].sched = &scheduler;
+    global_contexts[i].task_index = i;
+  }
 
-  // run_benchmark(8, 8, 10000);
+  std::cout << "[*] Spawning " << TOTAL_TASKS << " tasks...\n";
 
-  // ThreadPool<std::function<void()>> pool;
-  //
-  // pool.addTask([]() { std::cout << "Task 1 running on thread " <<
-  // std::this_thread::get_id() << std::endl; }); pool.addTask([]() { std::cout
-  // << "Task 2 running on thread " << std::this_thread::get_id() << std::endl;
-  // }); pool.addTask([]() { std::cout << "Task 3 running on thread " <<
-  // std::this_thread::get_id() << std::endl; });
-  //
+  // Start clock for total throughput
+  uint64_t total_start = rdtsc_start();
 
-  //
-  // run_unit_tests();
-  //
-  // std::cout << "starting simulation" << std::endl;
-  // // making shared ptr because we need to share it between our multiple
-  // threads std::shared_ptr<TSQueue<int>> q = std::make_shared<TSQueue<int>>();
-  //
-  // // to manage threads we are using a vector
-  // std::vector<std::thread> threads;
-  //
-  // for (int i = 1; i <= 100; i++) {
-  //   // thread by default copies the values,
-  //   // hence using std::ref to send by reference
-  //   std::thread thProducer(producer, std::ref(q), i * 3);
-  //   // we want actual threads to go in the vector hence std::move
-  //   threads.push_back(std::move(thProducer));
-  // }
-  //
-  // for (int i = 1; i <= 10; i++) {
-  //   std::thread thConsumer(consumer, std::ref(q), i * 7);
-  //   threads.push_back(std::move(thConsumer));
-  // }
-  //
-  // // joining all threads
-  // for (auto &th : threads) {
-  //   th.join();
-  // }
-  //
-  // std::cout << "TSQueue size: " << q->size() << std::endl;
+  // Spawn root task
+  global_contexts[0].spawn_tsc = total_start;
+  scheduler.start(cpu_heavy_task, &global_contexts[0]);
+
+  // Spin-wait lock-free
+  while (tasks_remaining.load(std::memory_order_acquire) > 0) {
+    _mm_pause();
+  }
+
+  uint64_t total_end = rdtsc_end();
+
+  scheduler.stop();
+
+  // Print Total Cycle Throughput
+  std::cout << "[+] Total Execution: " << (total_end - total_start)
+            << " CPU Cycles.\n";
+
+  // Print the Histogram (passing TOTAL_TASKS - 1 because we skip index 0)
+  print_latency_histogram(&global_latencies[1], TOTAL_TASKS - 1, 3.06);
+
+  // std::cout << "MutexBasedQueues: latency" << std::endl;
+  // MutexBasedLatencyBenchmark();
 
   return 0;
 }
